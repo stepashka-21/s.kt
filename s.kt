5f9076b666b56b0b2d6bfab72fa06d1ad5c3af0f
stepashka-21 <112618268+stepashka-21@users.noreply.github.com>
2022-09-19 08:28:35 +0300

    Update s.kt

--- s.kt
+++ s.kt
@@ -1,131 +1,78 @@
-import java.lang.Integer.min
+import java.lang.StringBuilder
+import kotlin.math.*
 
 /*
- * Реализуйте игру "Пьяница" (в простейшем варианте, на колоде в 36 карт)
- * https://ru.wikipedia.org/wiki/%D0%9F%D1%8C%D1%8F%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B8%D0%B3%D1%80%D0%B0)
- * Рука — это набор карт игрока. Карты выкладываются на стол из начала "рук" и сравниваются
- * только по значениям (масть игнорируется). При равных значениях сравниваются следующие карты.
- * Набор карт со стола перекладывается в конец руки победителя. Шестерка туза не бьёт.
- *
- * Реализация должна сопровождаться тестами.
+ * В этом задании необходимо написать программу, способную табулировать сразу несколько
+ * функций одной вещественной переменной на одном заданном отрезке.
  */
 
-// Размер колоды
-const val DECK_SIZE = 36
+typealias OneVariableFunction = (Double) -> Double
+typealias FunctionName = String
 
-// Масть
-enum class Suit {
-    diamonds, hearts, clubs, spades
+// Сформируйте набор как минимум из десяти вещественных функций одной переменной
+val availableFunctions : Map<FunctionName, OneVariableFunction> = mapOf(
+    "square" to { x -> x*x },
+    "sin" to { x -> sin(x) },
+    "sqrt" to { x -> sqrt(abs(x)) },
+    "cos" to { x -> cos(x) },
+    "tan" to { x -> tan(x) },
+    "log2" to { x -> log2(abs(x)) },
+    "log10" to { x -> log10(abs(x)) },
+    "atan" to { x -> atan(x) },
+    "sum5" to { x -> x+5 },
+    "lin4" to { x -> x*4 }
+)
+
+// Тип данных для представления входных данных
+data class InputData(val fromX: Double, val toX: Double, val numberOfPoints: Int,
+                     val functionNames: List<String>)
+
+// Чтение входных данных из параметров командной строки
+fun prepareData(args: Array<String>): InputData? {
+    val start: Double = args[0].toDouble()
+    val finish: Double = args[1].toDouble()
+    val count: Int = args[2].toInt()
+    val list = args.toMutableList().subList(3, args.size)
+    return InputData(start, finish, count, list)
 }
 
-// Значение
-enum class Rank (val value: Int){
-    six(1), seven(2), eight(3), nine(4), ten(5), jack(6), queen(7), king(8), ace(9)
-}
+// Тип данных для представления таблицы значений функций
+// с заголовками столбцов и строками (первый столбец --- значение x,
+// остальные столбцы --- значения функций). Одно из полей --- количество знаков
+// после десятичной точки.
+data class FunctionTable (val colf: Int) {
+    override fun toString(): String {
+        // Код, возвращающий строковое представление таблицы (с использованием StringBuilder)
+        // Столбец x выравнивается по левому краю, все остальные столбцы по правому.
+        // Для форматирования можно использовать функцию format или класс DecimalFormat
+        // (https://www.programiz.com/kotlin-programming/examples/round-number-decimal).
+        var str: StringBuilder
+        for (i in 0 until colf+1) {
 
-// Карта
-data class Card(val rank: Rank, val suit: Suit) {
-    val type = suit
-    val num = rank.value
-}
+        }
 
-val allrank = listOf("six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace")
-val allcard = listOf("diamonds", "hearts", "clubs", "spades")
-
-// Тип для обозначения игрока (первый, второй)
-enum class Player {
-    first, second
-}
-
-
-// Возвращается null, если значения карт совпадают
-fun roundWinner(card1: Card, card2 : Card): Player? {
-    if (card1.rank==card2.rank) {
-        return null
-    } else if (card1.rank<card2.rank) {
-        return Player.second
-    } else {
-        return Player.first
     }
 }
 
-// Колода
-typealias Deck = List<Card>
-
-// Набор карт у игрока
-typealias Hand = MutableList<Card>
-
-// Набор карт, выложенных на стол
-typealias Table = MutableList<Card>
-
-// Наборы карт у игроков
-typealias Hands = Map<Player, Hand>
-
-// Возвращает полную колоду (36 карт) в фиксированном порядке
-fun fullDeck() : Deck {
-    var k: List<Card> = emptyList()
-    for (rank in allrank)
-        for (card in allcard)
-            k+= Card(rank, card)
-    return k
-}
-
-// Раздача карт: случайное перемешивание (shuffle) и деление колоды пополам
-fun deal(deck: Deck) : Hands {
-    val shuffleDeck = deck.shuffled()
-    var hand1: Hand = mutableListOf<Card>()
-    var hand2: Hand = mutableListOf<Card>()
-    for (i in 1..deck.size)
-        if (i%2==1) {
-            hand1+=shuffleDeck[i-1]
-        } else {
-            hand2+=shuffleDeck[i-1]
-        }
-    return mapOf(Player.first to hand1, Player.second to hand2)
-}
-
-// Один раунд игры (в том числе спор при равных картах).
-// Возвращается победитель раунда и набор карт, выложенных на стол.
-fun round(hands: Hands): Pair<Player, List<Card>> {
-    var k1 = hands.get(Player.first) as MutableList
-    var k2 = hands.get(Player.second) as MutableList
-    var table = emptyList<Card>()
-    for (i in 0..min(k1.size, k2.size)) {
-        k1 -= k1[i]
-        k2 -= k2[i]
-        table += k1[i]
-        table += k2[i]
-        if (k1[i].num < k1[i].num) {
-            k1 += table
-            return Pair(Player.first, table)
-            break
-        } else if (k1[0].num > k2[0].num) {
-            k2 += table
-            return Pair(Player.second, table)
-            break
-        }
+/*
+ * Возвращает таблицу значений заданных функций на заданном отрезке [fromX, toX]
+ * с заданным количеством точек.
+ */
+fun tabulate(input: InputData): FunctionTable {
+    var s: String = ""
+    for (i in 0..k) {
+        s+=toString()
     }
-}
-
-// Полный цикл игры (возвращается победивший игрок)
-// в процессе игры печатаются ходы
-fun game(hands: Hands): Player {
-    var pl1 = hands.get(Player.first)
-    var pl2 = hands.get(Player.second)
-    var rund = 0
-    while (true)
-        rund+=1
-        var (win,table) = round(hands)
-        if (win!=null) {
-
-        }
 
 
 }
 
-fun main() {
-    val deck = fullDeck()
-    val hands = deal(deck) // Подумайте, почему val?
-    val winner = game(hands)
-    println("Победитель: $winner")
+fun main(args: Array<String>) {
+    // Входные данные принимаются в аргументах командной строки
+    // fromX fromY numberOfPoints function1 function2 function3 ...
+
+    val input = prepareData(args)
+
+    // Собственно табулирование и печать результата (здесь ошибка компиляции, необходимо исправить):
+    println(tabulate(input))
 }
